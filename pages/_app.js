ea5c665fdc2abb140d035a0895316ec3567ce170
thasocheat <thasocheat167@gmail>
2023-01-12 08:58:13 +0700

    update new file

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { Layout } from '../components';
 // import 'tailwindcss/tailwind.css';
 import '../styles/globals.scss';
-import type { AppProps } from 'next/app';
+// import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
