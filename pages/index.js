f9b0b28aa2e3a5e3e9fde7df55f921c8bf31a63e
thasocheat <thasocheat167@gmail>
2023-01-12 21:58:31 +0700

    text

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ return (
     <figure class="mt-10 h-auto max-w-lg mx-auto relative max-w-sm transition-all duration-300 filter grayscale hover:grayscale-0">
         <img class="rounded-xl" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" alt="image description"/>
         <figcaption class="absolute px-4 text-lg text-white bottom-6">
-            <p className='text-center'>រុំកិល mouse របស់អ្នកមកលើរូបនោះអ្នកនឹងដឹង ។</p>
+            <p className='text-center'>រំកិល mouse ឬ ប៉ះរូបប្រសិនបើអ្នកប្រើទូរស័ព្ទ មកលើរូបនោះអ្នកនឹងដឹង ។</p>
         </figcaption>
     </figure>
 
